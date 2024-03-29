ee56074dc0cc3f13eb683037de1b3d71d52e98fe
Itay Navon <83724543+ItayN137@users.noreply.github.com>
2024-03-29 17:32:56 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -60,6 +60,89 @@ const int IP_table[64] = {
     63, 55, 47, 39, 31, 23, 15, 7
 };
 
+static const int E_Table[48] = {
+    32,  1,  2,  3,  4,  5,
+     4,  5,  6,  7,  8,  9,
+     8,  9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32,  1
+};
+
+
+static const int S_Box[8][4][16] = {
+    // S-box 1
+    {
+        {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+        {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+        {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+        {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+    },
+    // S-box 2
+    {
+        {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+        {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+        {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+        {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
+    },
+    // S-box 3
+    {
+        {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+        {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+        {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+        {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
+    },
+    // S-box 4
+    {
+        {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+        {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+        {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+        {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}
+    },
+    // S-box 5
+    {
+        {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+        {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+        {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+        {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
+    },
+    // S-box 6
+    {
+        {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+        {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+        {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+        {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
+    },
+    // S-box 7
+    {
+        {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+        {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+        {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+        {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
+    },
+    // S-box 8
+    {
+        {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+        {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
+        {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+        {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
+    }
+};
+
+const int P_Table[32] = {
+    16,  7, 20, 21,
+    29, 12, 28, 17,
+     1, 15, 23, 26,
+     5, 18, 31, 10,
+     2,  8, 24, 14,
+    32, 27,  3,  9,
+    19, 13, 30,  6,
+    22, 11,  4, 25
+};
+
+
 
 /// <summary>
 /// Displaying hexadecimal based number in binary based number.
@@ -337,6 +420,103 @@ const char* hex_to_binary_char(char hex) {
 }
 
 
+char* binary_xor(const char* num1, const char* num2, size_t length) {
+    char* result = (char*)malloc(length + 1);
+    if (result == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    for (size_t i = 0; i < length; ++i) {
+        result[i] = ((num1[i] - '0') ^ (num2[i] - '0')) + '0';
+    }
+
+    result[length] = '\0';
+
+    return result;
+}
+
+char* apply_permutation_p(char* data) {
+    char* p_data = (char*)malloc(33 * sizeof(char));
+    if (p_data == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    for (int i = 0; i < 32; i++) {
+        p_data[i] = data[P_Table[i] - 1];
+        //printf("number to apply in round %d: %d --> %d\n", i, P_Table[i] - 1, data[P_Table[i] - 1] - '0');
+    }
+    p_data[32] = '\0';
+    return p_data;
+}
+
+
+char* apply_s_boxes(char* data) {
+    char* s_data = (char*)calloc(33, sizeof(char));
+    if (s_data == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    int row, column;
+    for (int i = 0; i < 8; i++) {
+        row = (data[i*6] -'0') * 2 + (data[(i * 6) + 5] - '0');
+        column = (data[(i * 6) + 1] - '0') * 8 + (data[(i * 6) + 2] - '0') * 4 + (data[(i * 6) + 3] - '0') * 2 + (data[(i * 6) + 4] - '0');
+        strcat(s_data,hex_to_binary_table[S_Box[i][row][column]]);
+    }
+    s_data[32] = '\0';
+    //printf("\ndata after s box: %s", s_data);
+    return s_data;
+}
+
+char* expension(char* data) {
+    char* expanded_data = (char*)malloc(49 * sizeof(char));
+    if (expanded_data == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    for (int i = 0; i < 48; i++) {
+        expanded_data[i] = data[E_Table[i] - 1];
+    }
+    expanded_data[48] = '\0';
+    return expanded_data;
+}
+
+
+char* encryption_rounds(char* data, char** keys) {
+    char* right_data = (char*)malloc(33*sizeof(char));
+    char* left_data = (char*)malloc(33*sizeof(char));
+    char* next_left_data = (char*)malloc(33*sizeof(char));
+
+    if (right_data == NULL || left_data == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+
+    memcpy(left_data, data, 32);
+    left_data[32] = '\0';
+    memcpy(right_data, data + 32, 32);
+    right_data[32] = '\0';
+
+    strcpy(next_left_data, right_data);
+    printf("next left data: %s \n", next_left_data);
+
+    right_data = (char*)realloc(right_data, 49 * sizeof(char));
+    right_data = expension(right_data);
+    printf("%s \n", right_data);
+    right_data = binary_xor(right_data, keys[0], 48);
+    printf("%s \n", right_data);
+    char* s_right_data = apply_s_boxes(right_data);
+    printf("%s \n", s_right_data);
+    char* p_right_data = apply_permutation_p(s_right_data);
+    printf("permutation p:%s \n", p_right_data);
+    char* next_data = binary_xor(p_right_data, left_data, 32);
+    printf("R1: %s \n", next_data);
+    strncpy(left_data, next_left_data, 32);
+    printf("%s \n", left_data);
+    return right_data;
+}
+
+
+
 
 
 void printRows(char** array, int numRows) {
@@ -430,11 +610,17 @@ int main() {
     printf("%d \n", strlen(binaryData));
     dataArr = create_blocks_from_data(binaryData, chunks);
     printf("%s", "before IP: \n");
-    
     printRows(dataArr, chunks);
+
+
+    //// -----------------------encryption part--------------------------
+
     ipData = apply_IP_to_data_array(dataArr, chunks);
     printf("%s", "after IP: \n");
     printRows(ipData, chunks);
+
+    char* data1 = (char*)malloc(49 * sizeof(char));
+    data1 = encryption_rounds(ipData[0], pc2_keys_arr);
 
 
 
